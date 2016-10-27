57032abaa1aad96c93cf60c52696f0326d5ed9e5
Rick Boss <rickeboss@gmail.com>
2016-10-26 20:52:00 -0700

    Add placeholder for day03
    It's already done, but I can't share the source until after the 1st.

---
+++ other-projects/Day03/main.cpp
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main(int argc, char** argv) {
+    puts("Finding the optimal arrangements of points in a circle using an evolutionary algorithm.\n");
+    puts("This is a placeholder for Day 3. Unfortunately, it's time sensitive and can't be shared until after November 1st at 5:00pm. I'll post it here then!");
+
+   return 0; 
+}
